41d53cd2181a8e8614ffd086d2a0d73eaca802b1
nisahabiba <nisa.habiba@live.iium.edu.my>
2023-07-28 16:41:45 +0800

    edited UI - regist_page & succeed_page

--- lib/auth/pages/register_page.dart
+++ lib/auth/pages/register_page.dart
@@ -34,40 +34,60 @@ class RegisterPage extends StatelessWidget {
               height: 0.3.sh,
             ),
             DefaultTextStyle(
-                style: GoogleFonts.averiaGruesaLibre(color: Colors.white),
-                child: Container(
-                  padding: EdgeInsets.only(
-                    left: 10.r,
-                    right: 10.r,
-                  ),
-                  height: 0.65.sh,
-                  child: Stack(
-                    alignment: AlignmentDirectional.topCenter,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/gradient_card.svg',
-                        width: 1.sw,
+              style: GoogleFonts.averiaGruesaLibre(color: Colors.black),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 10.r,
+                  right: 10.r,
+                ),
+                height: 0.65.sh,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 93.h,
+                      width: 290.w,
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 3, color: Colors.black),
+                          color: Color(0xffDFDCEF),
+                          borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              topLeft: Radius.circular(20))),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Register Yourself',
+                              style: TextStyle(
+                                fontSize: 32.sp,
+                              ),
+                            ),
+                            Text(
+                              "to start a new journey",
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                              ),
+                            ),
+                          ]),
+                    ),
+                    Container(
+                      height: 0.45.sh,
+                      padding: EdgeInsets.only(
+                        left: 10.r,
+                        right: 10.r,
                       ),
-                      ListView(),
-                      Column(
+                      width: 290.w,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 3, color: Colors.black),
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.only(
+                              bottomRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(20))),
+                      child: Column(
                         children: [
                           SizedBox(
-                            height: 40.h,
-                          ),
-                          Text(
-                            'Register Yourself',
-                            style: TextStyle(
-                              fontSize: 32.sp,
-                            ),
-                          ),
-                          Text(
-                            "to start a new journey",
-                            style: TextStyle(
-                              fontSize: 20.sp,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.h,
+                            height: 10.h,
                           ),
                           AuthFormComponent(
                             formKey: 'name',
@@ -103,25 +123,27 @@ class RegisterPage extends StatelessWidget {
                             textInputType: TextInputType.number,
                           ),
                           SizedBox(
-                            height: 20.h,
+                            height: 10.h,
                           ),
                           SizedBox(
                             height: 38.h,
                             width: 0.8.sw,
                             child: ElevatedButton(
                               style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xff70303A)),
+                                  backgroundColor: Color(0xffFAC6EA),
+                                  side: BorderSide(
+                                      width: 3.0, color: Colors.black)),
                               onPressed: () {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SucceedPage()));
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => SucceedPage(),
+                                  ),
+                                );
                               },
                               child: const Text(
-                                'Sign Up',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
+                                'Login',
+                                style: TextStyle(color: Colors.black),
                               ),
                             ),
                           ),
@@ -132,9 +154,9 @@ class RegisterPage extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                '*if you dont have created any account, please',
+                                '*if you have already created any account, please',
                                 style: TextStyle(
-                                  color: const Color(0xff70303A),
+                                  color: Colors.black,
                                   fontSize: 9.sp,
                                 ),
                               ),
@@ -146,9 +168,9 @@ class RegisterPage extends StatelessWidget {
                                           builder: (context) => LoginPage()));
                                 },
                                 child: Text(
-                                  'Login',
+                                  'Log In',
                                   style: TextStyle(
-                                    color: const Color(0xffCB442B),
+                                    color: Colors.black,
                                     fontSize: 9.sp,
                                     fontWeight: FontWeight.w600,
                                     decoration: TextDecoration.underline,
@@ -158,10 +180,141 @@ class RegisterPage extends StatelessWidget {
                             ],
                           )
                         ],
-                      )
-                    ],
-                  ),
-                ))
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+            // DefaultTextStyle(
+            //     style: GoogleFonts.averiaGruesaLibre(color: Colors.white),
+            //     child: Container(
+            //       padding: EdgeInsets.only(
+            //         left: 10.r,
+            //         right: 10.r,
+            //       ),
+            //       height: 0.65.sh,
+            //       child: Stack(
+            //         alignment: AlignmentDirectional.topCenter,
+            //         children: [
+            //           SvgPicture.asset(
+            //             'assets/gradient_card.svg',
+            //             width: 1.sw,
+            //           ),
+            //           ListView(),
+            //           Column(
+            //             children: [
+            //               SizedBox(
+            //                 height: 40.h,
+            //               ),
+            //               Text(
+            //                 'Register Yourself',
+            //                 style: TextStyle(
+            //                   fontSize: 32.sp,
+            //                 ),
+            //               ),
+            //               Text(
+            //                 "to start a new journey",
+            //                 style: TextStyle(
+            //                   fontSize: 20.sp,
+            //                 ),
+            //               ),
+            //               SizedBox(
+            //                 height: 20.h,
+            //               ),
+            //               AuthFormComponent(
+            //                 formKey: 'name',
+            //                 controller: nameCtr,
+            //                 hintText: 'Your Name',
+            //                 textInputType: TextInputType.text,
+            //               ),
+            //               SizedBox(
+            //                 height: 20.h,
+            //               ),
+            //               AuthFormComponent(
+            //                 formKey: 'email',
+            //                 controller: emailCtr,
+            //                 hintText: 'Your Email',
+            //                 textInputType: TextInputType.emailAddress,
+            //               ),
+            //               SizedBox(
+            //                 height: 20.h,
+            //               ),
+            //               AuthFormComponent(
+            //                 formKey: 'password',
+            //                 controller: passwordCtr,
+            //                 hintText: 'Set your password',
+            //                 textInputType: TextInputType.number,
+            //               ),
+            //               SizedBox(
+            //                 height: 20.h,
+            //               ),
+            //               AuthFormComponent(
+            //                 formKey: 'confirm_password',
+            //                 controller: confirmPasswordCtr,
+            //                 hintText: 'Confirm your password',
+            //                 textInputType: TextInputType.number,
+            //               ),
+            //               SizedBox(
+            //                 height: 20.h,
+            //               ),
+            //               SizedBox(
+            //                 height: 38.h,
+            //                 width: 0.8.sw,
+            //                 child: ElevatedButton(
+            //                   style: ElevatedButton.styleFrom(
+            //                       backgroundColor: const Color(0xff70303A)),
+            //                   onPressed: () {
+            //                     Navigator.pushReplacement(
+            //                         context,
+            //                         MaterialPageRoute(
+            //                             builder: (context) => SucceedPage()));
+            //                   },
+            //                   child: const Text(
+            //                     'Sign Up',
+            //                     style: TextStyle(
+            //                       color: Colors.white,
+            //                     ),
+            //                   ),
+            //                 ),
+            //               ),
+            //               SizedBox(
+            //                 height: 15.h,
+            //               ),
+            //               Row(
+            //                 mainAxisAlignment: MainAxisAlignment.center,
+            //                 children: [
+            //                   Text(
+            //                     '*if you dont have created any account, please',
+            //                     style: TextStyle(
+            //                       color: const Color(0xff70303A),
+            //                       fontSize: 9.sp,
+            //                     ),
+            //                   ),
+            //                   GestureDetector(
+            //                     onTap: () {
+            //                       Navigator.push(
+            //                           context,
+            //                           MaterialPageRoute(
+            //                               builder: (context) => LoginPage()));
+            //                     },
+            //                     child: Text(
+            //                       'Login',
+            //                       style: TextStyle(
+            //                         color: const Color(0xffCB442B),
+            //                         fontSize: 9.sp,
+            //                         fontWeight: FontWeight.w600,
+            //                         decoration: TextDecoration.underline,
+            //                       ),
+            //                     ),
+            //                   ),
+            //                 ],
+            //               )
+            //             ],
+            //           )
+            //         ],
+            //       ),
+            //     ))
           ],
         ),
       ),
--- lib/auth/pages/succeed_signup.dart
+++ lib/auth/pages/succeed_signup.dart
@@ -28,55 +28,81 @@ class SucceedPage extends StatelessWidget {
             height: 0.3.sh,
           ),
           DefaultTextStyle(
-              style: GoogleFonts.averiaGruesaLibre(color: Colors.white),
-              child: Container(
-                padding: EdgeInsets.only(
-                  left: 10.r,
-                  right: 10.r,
-                ),
-                height: 0.65.sh,
-                child: Stack(
-                  alignment: AlignmentDirectional.topCenter,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/gradient_card.svg',
-                      width: 1.sw,
+            style: GoogleFonts.averiaGruesaLibre(color: Colors.black),
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 10.r,
+                right: 10.r,
+              ),
+              height: 0.65.sh,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 290.h,
+                    width: 290.w,
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 3, color: Colors.black),
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'You are in!',
+                            style: TextStyle(
+                              fontSize: 32.sp,
+                            ),
+                          ),
+                          Text(
+                            "Login to start your journey",
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                            ),
+                          ),
+                        ]),
+                  ),
+                  Container(
+                    height: 90.h,
+                    padding: EdgeInsets.only(
+                      left: 10.r,
+                      right: 10.r,
                     ),
-                    Column(
+                    width: 290.w,
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 3, color: Colors.black),
+                        color: Color(0xffDFDCEF),
+                        borderRadius: const BorderRadius.only(
+                            bottomRight: Radius.circular(20),
+                            bottomLeft: Radius.circular(20))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
-                          height: 80.h,
+                          height: 10.h,
                         ),
-                        Text(
-                          'You are in!',
-                          style: TextStyle(
-                            fontSize: 32.sp,
-                          ),
-                        ),
-                        Text(
-                          "Login to start your journey",
-                          style: TextStyle(
-                            fontSize: 20.sp,
-                          ),
-                        ),
-                        SizedBox(height: 20.h),
                         SizedBox(
                           height: 38.h,
                           width: 0.8.sw,
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
-                                backgroundColor: const Color(0xff70303A)),
+                                backgroundColor: Color(0xffFAC6EA),
+                                side: BorderSide(
+                                    width: 3.0, color: Colors.black)),
                             onPressed: () {
                               Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => LoginPage()));
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginPage(),
+                                ),
+                              );
                             },
                             child: const Text(
                               'Login',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
+                              style: TextStyle(color: Colors.black),
                             ),
                           ),
                         ),
@@ -84,10 +110,73 @@ class SucceedPage extends StatelessWidget {
                           height: 15.h,
                         ),
                       ],
-                    )
-                  ],
-                ),
-              ))
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+          // DefaultTextStyle(
+          //     style: GoogleFonts.averiaGruesaLibre(color: Colors.white),
+          //     child: Container(
+          //       padding: EdgeInsets.only(
+          //         left: 10.r,
+          //         right: 10.r,
+          //       ),
+          //       height: 0.65.sh,
+          //       child: Stack(
+          //         alignment: AlignmentDirectional.topCenter,
+          //         children: [
+          //           SvgPicture.asset(
+          //             'assets/gradient_card.svg',
+          //             width: 1.sw,
+          //           ),
+          //           Column(
+          //             children: [
+          //               SizedBox(
+          //                 height: 80.h,
+          //               ),
+          //               Text(
+          //                 'You are in!',
+          //                 style: TextStyle(
+          //                   fontSize: 32.sp,
+          //                 ),
+          //               ),
+          //               Text(
+          //                 "Login to start your journey",
+          //                 style: TextStyle(
+          //                   fontSize: 20.sp,
+          //                 ),
+          //               ),
+          //               SizedBox(height: 20.h),
+          //               SizedBox(
+          //                 height: 38.h,
+          //                 width: 0.8.sw,
+          //                 child: ElevatedButton(
+          //                   style: ElevatedButton.styleFrom(
+          //                       backgroundColor: const Color(0xff70303A)),
+          //                   onPressed: () {
+          //                     Navigator.push(
+          //                         context,
+          //                         MaterialPageRoute(
+          //                             builder: (context) => LoginPage()));
+          //                   },
+          //                   child: const Text(
+          //                     'Login',
+          //                     style: TextStyle(
+          //                       color: Colors.white,
+          //                     ),
+          //                   ),
+          //                 ),
+          //               ),
+          //               SizedBox(
+          //                 height: 15.h,
+          //               ),
+          //             ],
+          //           )
+          //         ],
+          //       ),
+          //     ))
         ],
       )),
     ));
