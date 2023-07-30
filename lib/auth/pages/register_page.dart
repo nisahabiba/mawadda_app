326d878d17285a831e0c4e07587a9f7ff21fa1ee
nisahabiba <nisa.habiba@live.iium.edu.my>
2023-07-30 11:26:21 +0800

    improved UI homepage

--- lib/auth/pages/register_page.dart
+++ lib/auth/pages/register_page.dart
@@ -205,7 +205,10 @@ class RegisterPage extends StatelessWidget {
                                 ),
                               ),
                             ],
-                          )
+                          ),
+                          SizedBox(
+                            height: 15.h,
+                          ),
                         ],
                       ),
                     )
@@ -213,135 +216,6 @@ class RegisterPage extends StatelessWidget {
                 ),
               ),
             )
-            // DefaultTextStyle(
-            //     style: GoogleFonts.averiaGruesaLibre(color: Colors.white),
-            //     child: Container(
-            //       padding: EdgeInsets.only(
-            //         left: 10.r,
-            //         right: 10.r,
-            //       ),
-            //       height: 0.65.sh,
-            //       child: Stack(
-            //         alignment: AlignmentDirectional.topCenter,
-            //         children: [
-            //           SvgPicture.asset(
-            //             'assets/gradient_card.svg',
-            //             width: 1.sw,
-            //           ),
-            //           ListView(),
-            //           Column(
-            //             children: [
-            //               SizedBox(
-            //                 height: 40.h,
-            //               ),
-            //               Text(
-            //                 'Register Yourself',
-            //                 style: TextStyle(
-            //                   fontSize: 32.sp,
-            //                 ),
-            //               ),
-            //               Text(
-            //                 "to start a new journey",
-            //                 style: TextStyle(
-            //                   fontSize: 20.sp,
-            //                 ),
-            //               ),
-            //               SizedBox(
-            //                 height: 20.h,
-            //               ),
-            //               AuthFormComponent(
-            //                 formKey: 'name',
-            //                 controller: nameCtr,
-            //                 hintText: 'Your Name',
-            //                 textInputType: TextInputType.text,
-            //               ),
-            //               SizedBox(
-            //                 height: 20.h,
-            //               ),
-            //               AuthFormComponent(
-            //                 formKey: 'email',
-            //                 controller: emailCtr,
-            //                 hintText: 'Your Email',
-            //                 textInputType: TextInputType.emailAddress,
-            //               ),
-            //               SizedBox(
-            //                 height: 20.h,
-            //               ),
-            //               AuthFormComponent(
-            //                 formKey: 'password',
-            //                 controller: passwordCtr,
-            //                 hintText: 'Set your password',
-            //                 textInputType: TextInputType.number,
-            //               ),
-            //               SizedBox(
-            //                 height: 20.h,
-            //               ),
-            //               AuthFormComponent(
-            //                 formKey: 'confirm_password',
-            //                 controller: confirmPasswordCtr,
-            //                 hintText: 'Confirm your password',
-            //                 textInputType: TextInputType.number,
-            //               ),
-            //               SizedBox(
-            //                 height: 20.h,
-            //               ),
-            //               SizedBox(
-            //                 height: 38.h,
-            //                 width: 0.8.sw,
-            //                 child: ElevatedButton(
-            //                   style: ElevatedButton.styleFrom(
-            //                       backgroundColor: const Color(0xff70303A)),
-            //                   onPressed: () {
-            //                     Navigator.pushReplacement(
-            //                         context,
-            //                         MaterialPageRoute(
-            //                             builder: (context) => SucceedPage()));
-            //                   },
-            //                   child: const Text(
-            //                     'Sign Up',
-            //                     style: TextStyle(
-            //                       color: Colors.white,
-            //                     ),
-            //                   ),
-            //                 ),
-            //               ),
-            //               SizedBox(
-            //                 height: 15.h,
-            //               ),
-            //               Row(
-            //                 mainAxisAlignment: MainAxisAlignment.center,
-            //                 children: [
-            //                   Text(
-            //                     '*if you dont have created any account, please',
-            //                     style: TextStyle(
-            //                       color: const Color(0xff70303A),
-            //                       fontSize: 9.sp,
-            //                     ),
-            //                   ),
-            //                   GestureDetector(
-            //                     onTap: () {
-            //                       Navigator.push(
-            //                           context,
-            //                           MaterialPageRoute(
-            //                               builder: (context) => LoginPage()));
-            //                     },
-            //                     child: Text(
-            //                       'Login',
-            //                       style: TextStyle(
-            //                         color: const Color(0xffCB442B),
-            //                         fontSize: 9.sp,
-            //                         fontWeight: FontWeight.w600,
-            //                         decoration: TextDecoration.underline,
-            //                       ),
-            //                     ),
-            //                   ),
-            //                 ],
-            //               )
-            //             ],
-            //           )
-            //         ],
-            //       ),
-            //     ))
           ],
         ),
       ),
