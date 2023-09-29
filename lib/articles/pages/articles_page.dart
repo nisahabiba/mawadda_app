08f30a1cbab9f6abd7d1de3e7a764dc89a356a83
nisahabiba <nisa.habiba@live.iium.edu.my>
2023-09-29 18:03:04 +0800

    update articles page

--- lib/articles/pages/articles_page.dart
+++ lib/articles/pages/articles_page.dart
@@ -40,12 +40,60 @@ class ArticlesPage extends StatelessWidget {
                 color: Colors.white,
                 shape: const RoundedRectangleBorder(
                     side: BorderSide(color: Colors.black, width: 3),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: Container(
-                  width: 1.sw,
-                  height: 150.h,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      width: 1.sw,
+                      height: 210.h,
+                      child: Image.asset(
+                        "assets/back_article_header.png",
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Container(
+                        padding: EdgeInsets.only(left: 20.r),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 130.h,
+                            ),
+                            Text(
+                              "Beyond The Words “I do”",
+                              style: GoogleFonts.averiaGruesaLibre(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFB4B0CE),
+                                  shape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: Colors.black, width: 3),
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {},
+                              child: Text(
+                                'by Shahina Siddiqui',
+                                style: GoogleFonts.averiaGruesaLibre(
+                                    color: Colors.black, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        )),
+                  ],
                 )),
           ),
+          Container(
+            width: 1.sw,
+            padding: EdgeInsets.only(left: 20.r),
+            child: const Text("More Articles",
+                textAlign: TextAlign.left, style: TextStyle(fontSize: 24)),
+          ),
+
+//--------------------> Article 1
+
           Container(
             padding: EdgeInsets.only(top: 10.r, left: 10.r, right: 10.r),
             child: Stack(
@@ -64,14 +112,164 @@ class ArticlesPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const Icon(
-                          Icons.check_circle,
-                          size: 40.0,
+                        Image.asset(
+                          "assets/pic_article1.png",
                         ),
                         SizedBox(
-                          width: 10.w,
+                          width: 40.w,
                         ),
-                        const Text("text"),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Fundamentals of\na Happy Marriage",
+                              style: GoogleFonts.averiaGruesaLibre(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFB4B0CE),
+                                  shape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: Colors.black, width: 3),
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {},
+                              child: Text(
+                                'by Shahina Siddiqui',
+                                style: GoogleFonts.averiaGruesaLibre(
+                                    color: Colors.black, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+//--------------------> Article 2
+
+          Container(
+            padding: EdgeInsets.only(top: 10.r, left: 10.r, right: 10.r),
+            child: Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                Card(
+                  elevation: 0,
+                  color: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black, width: 3),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Container(
+                    width: 1.sw,
+                    height: 100.h,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/pic_article2.png",
+                        ),
+                        SizedBox(
+                          width: 40.w,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Confessions of a\nShaikh’s Wife Part 1:\nWhat Makes the Man",
+                              style: GoogleFonts.averiaGruesaLibre(
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFB4B0CE),
+                                  shape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: Colors.black, width: 3),
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {},
+                              child: Text(
+                                'by Umm Fudayl',
+                                style: GoogleFonts.averiaGruesaLibre(
+                                    color: Colors.black, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+//--------------------> Article 3
+
+          Container(
+            padding: EdgeInsets.only(top: 10.r, left: 10.r, right: 10.r),
+            child: Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                Card(
+                  elevation: 0,
+                  color: Colors.white,
+                  shape: const RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black, width: 3),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Container(
+                    width: 1.sw,
+                    height: 100.h,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/pic_article3.png",
+                        ),
+                        SizedBox(
+                          width: 40.w,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Pre–nups Protect\nMuslim Women",
+                              style: GoogleFonts.averiaGruesaLibre(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline),
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFB4B0CE),
+                                  shape: RoundedRectangleBorder(
+                                      side: const BorderSide(
+                                          color: Colors.black, width: 3),
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {},
+                              child: Text(
+                                'by Aisyah Sulaiman',
+                                style: GoogleFonts.averiaGruesaLibre(
+                                    color: Colors.black, fontSize: 12),
+                              ),
+                            )
+                          ],
+                        )
                       ],
                     ),
                   ),
