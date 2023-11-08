c5612979f1e3f0d8bb64301d5b4caf407c01b9d2
Abi Mulya <abimulya.2001@gmail.com>
2023-11-08 19:41:01 +0700

    implement login and register with firebase

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,30 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({
+    required this.message,
+  });
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+
+  @override
+  bool get stringify => true;
+}
+
+//* RemoteFailure
+class RemoteFailure extends Failure {
+  const RemoteFailure({
+    required super.message,
+  });
+}
+
+//* NoInternetFailure
+class NoInternetFailure extends Failure {
+  const NoInternetFailure()
+      : super(
+          message: 'Tidak ada koneksi internet!',
+        );
+}
